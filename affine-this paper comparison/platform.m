008f2a97ccfe2ff1973e600400928ef4af64e55f
Oliver <37354467+Dzalwaysc@users.noreply.github.com>
2022-07-30 17:15:59 +0800

    Update platform.m

--- affine-this paper comparison/platform.m
+++ affine-this paper comparison/platform.m
@@ -1,24 +1,7 @@
 function [eta, etad, V, C__, D__, M__] = platform(state, tao, step, t)
     % start stage
     dstate = plant(t, state, tao);
-    K1 = dstate;
-    
-    % middle stage
-    state_ = state + (step/2)*K1;
-    dstate_ = plant(t, state_, tao);
-    K2 = dstate_;
-    
-    % middle stage
-    state_ = state + (step/2)*K2;
-    dstate_ = plant(t, state_, tao);
-    K3 = dstate_;
-    
-    % final stage
-    state_ = state + (step/2)*K3;
-    dstate_ = plant(t, state_, tao);
-    K4 = dstate_;
-    
-    state = state + (step/6)*(K1+2*K2+2*K3+K4);
+    state = state + step*dstate;
     
     eta = state(1:3); phi = eta(3);
     V = state(4:6); u = V(1); v = V(2); r = V(3);
