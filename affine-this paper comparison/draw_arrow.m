955f8b5ac633f02fcb75e658a93a9e109f943ce8
Oliver <37354467+Dzalwaysc@users.noreply.github.com>
2022-07-30 17:12:43 +0800

    Delete affine-this paper comparison directory

--- affine-this paper comparison/draw_arrow.m
+++
@@ -1,5 +0,0 @@
-function [] = draw_arrow(Pos, Yaw, len, color)
-    startPos = Pos;
-    endPos = [Pos(1)+len*cos(Yaw), Pos(2)+len*sin(Yaw)];
-    line([startPos(1),endPos(1)], [startPos(2),endPos(2)], 'Color', color);
-end
